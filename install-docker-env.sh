805cf0d5bd656bec8df6bb913b7a5df478356027
JackLicc <44892266+JackLicc@users.noreply.github.com>
2025-01-23 16:49:06 +1300

    add mailpit

--- install-docker-env.sh
+++ install-docker-env.sh
@@ -3,17 +3,19 @@
 [ ! -d "codebases/hyperwaiver" ] && git clone --branch main git@github.com:JackLicc/hyperwaiver.git codebases/hyperwaiver
 [ ! -d "codebases/hyperwaiver-fe" ] && git clone --branch main git@github.com:JackLicc/hyperwaiver-fe.git codebases/hyperwaiver-fe
 
-docker compose up --no-start
+if [ "$ENV" = "production" ]; then
+    compose_files="-f docker-compose.yaml -f docker-compose.prod.yaml"
+else
+    compose_files="-f docker-compose.yaml -f docker-compose.dev.yaml"
+fi
 
-docker compose up --detach
+docker compose $compose_files up --no-start
+docker compose $compose_files up --detach
 sleep 60
-
 docker exec -it mariadb bash -c "chmod 444 /etc/mysql/my.cnf"
-
-docker compose -f ./docker-compose.yaml stop
+docker compose $compose_files stop
 sleep 20
-
-docker compose -f ./docker-compose.yaml up --detach
+docker compose $compose_files up --detach
 sleep 60
 
 echo "Environment created ok. Run start.sh to run."
