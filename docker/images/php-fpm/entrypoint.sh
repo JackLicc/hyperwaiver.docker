7102d0e3d9c7fb0429841cf2a54e795d823c3bcf
JackLicc <44892266+JackLicc@users.noreply.github.com>
2025-02-26 10:15:06 +1300

    bugfix

--- docker/images/php-fpm/entrypoint.sh
+++ docker/images/php-fpm/entrypoint.sh
@@ -4,16 +4,11 @@
 UID=${HOST_UID:-1000}
 GID=${HOST_GID:-1000}
 
-RUN usermod -u "$UID" www-data
-RUN groupmod -g "$GID" www-data
+usermod -u "$UID" www-data
+groupmod -g "$GID" www-data
 
-RUN chown -R www-data:www-data /var/www/hyperwaiver \
+chown -R www-data:www-data /var/www/hyperwaiver \
     && mkdir -p /var/www/hyperwaiver/storage \
     && chmod -R 755 /var/www/hyperwaiver/storage
-
-
-USER www-data
-
-EXPOSE 9000
 
 exec gosu www-data "$@"
